fc87d4d3bb7d1d60e227e281953ac3bb87649ca4
vpayagon <96186822+vpayagon@users.noreply.github.com>
2022-02-16 18:36:14 +0530

    HomepageTest updated for DD approach

--- src/test/java/com/DDdemo/testcases/HomePageTest.java
+++ src/test/java/com/DDdemo/testcases/HomePageTest.java
@@ -3,23 +3,24 @@ package com.DDdemo.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.DDdemo.base.TestBase;
 import com.DDdemo.pages.HomePage;
 import com.DDdemo.pages.JavaPage;
+import com.DDdemo.util.TestUtil;
 
 public class HomePageTest extends TestBase {
 
 	JavaPage javapage;
 	HomePage homepage;
-	
-	static boolean assertVerify=false;
-	public static String TestDataSheetPath = ".\\src\\main\\java\\com\\DDdemo\\testdata\\datavalues.xlsx";
-	String sheetName="values";
-	int rowNum=2;
 
-	
+	static boolean assertVerify = false;
+	public static String TestDataSheetPath = ".\\src\\main\\java\\com\\DDdemo\\testdata\\datavalues.xlsx";
+	String sheetName = "values";
+	int rowNum = 2;
+
 	public HomePageTest() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -32,32 +33,40 @@ public class HomePageTest extends TestBase {
 
 	}
 
-	@Test(priority = 1)
-	public void writeFieldDDfmk() {
-		Assert.assertTrue(homepage.writeonField(TestDataSheetPath, sheetName, rowNum));
-		
+	@DataProvider
+	public Object[][] getData() {
+		Object data[][] = TestUtil.getTestData(TestDataSheetPath, sheetName);
+		return data;
+	}
+
+	@Test(priority = 1, dataProvider = "getData")
+	public void writeFieldDDfmk(String value) throws InterruptedException {
+		homepage.writeonFieldDDapproach(value);
+		Assert.assertTrue(homepage.clicksearch());
+		Thread.sleep(3000);
+
 	}
 
 	@Test(priority = 2)
 	public void ClickActionTest1() throws InterruptedException {
 		Assert.assertTrue(homepage.clicksearch());
-			}
-	
+	}
+
 	@Test(priority = 3)
 	public void ClickActiononImgTest2() throws InterruptedException {
 		Assert.assertTrue(homepage.clickImg());
-			}
-	
+	}
+
 	@Test(priority = 4)
 	public void ClickActionLinkTest3() throws InterruptedException {
 		Assert.assertTrue(homepage.clickLinkJava());
-			}
-	
+	}
+
 	@Test(priority = 5)
 	public void verifyTest() throws InterruptedException {
 		Assert.assertTrue(homepage.verifyText());
-			}
-	
+	}
+
 	@AfterClass
 	public void teardown() {
 		driver.quit();
