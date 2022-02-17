412e0cd99868e057e609523167b3c402c012fd7c
vpayagon <96186822+vpayagon@users.noreply.github.com>
2022-02-17 18:10:10 +0530

    Update TestBase.java

--- src/main/java/com/DDdemo/base/TestBase.java
+++ src/main/java/com/DDdemo/base/TestBase.java
@@ -14,14 +14,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
-
-
 public class TestBase {
 
 	public static WebDriver driver;
 	public static Properties prop;
 	public static EventFiringWebDriver e_driver;
-	
+
 	public TestBase() {
 		// TODO Auto-generated constructor stub
 		// Read properties file
@@ -39,52 +37,21 @@ public class TestBase {
 
 	public static void initialization() {
 		ChromeOptions options = new ChromeOptions();
-		String browserName = prop.getProperty("browser");
 		String applicationurl = prop.getProperty("applicationurl");
-		//String applicationurl = System.getProperty("applicationurl");
-		String osName = System.getProperty("os.name");
-		if (browserName.equals("chrome")) {
-			if (osName.equalsIgnoreCase("Windows 10")) {
-				System.setProperty("webdriver.chrome.driver", "./src/main/java/com/DDdemo/resources/chromedriver.exe");
-				DesiredCapabilities caps = DesiredCapabilities.chrome();
-				caps.setCapability(ChromeOptions.CAPABILITY, options);
-				caps.setCapability("acceptInsecureCerts", true);
-				driver = new ChromeDriver(caps);
-				driver.manage().window().maximize();
-				driver.manage().deleteAllCookies();
-				driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// String applicationurl = System.getProperty("applicationurl");
 
-			} else {
-				System.setProperty("webdriver.chrome.driver", "./src/main/java/com/pgmp/resources/chromedriver");
-				options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200",
-						"--ignore-certificate-errors", "--silent");
-				options.addArguments("--no-sandbox");
-				options.addArguments("--disable-dev-shm-usage");
-				driver = new ChromeDriver(options);
-				driver.manage().window().maximize();
-				driver.manage().deleteAllCookies();
+		System.setProperty("webdriver.chrome.driver", "./src/main/java/com/DDdemo/resources/chromedriver.exe");
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability(ChromeOptions.CAPABILITY, options);
+		caps.setCapability("acceptInsecureCerts", true);
+		driver = new ChromeDriver(caps);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-			}
-
-		}
 		// Automation execution on Fire fox browser
-		else if (browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "./src/main/java/com/pgmp/resources/geckodriver.exe");
-			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			capabilities.setCapability("marionette", true);
-			driver = new FirefoxDriver(capabilities);
-		}
 
-		else if (browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", "./src/main/java/com/pgmp/resources/msedgedriver.exe");
-			driver = new EdgeDriver();
-			driver.manage().window().maximize();
-			driver.manage().deleteAllCookies();
-			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-		}
 		// uncomment this later for logging information
 		// Detailed logging on the console - EventFiringWebDriver
 		// e_driver = new EventFiringWebDriver(driver);
@@ -98,8 +65,10 @@ public class TestBase {
 			System.out.println("test url value=====" + prop.getProperty("testurl"));
 			driver.get(prop.getProperty("testurl"));
 			System.out.println("after driver get property");
-			/*driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);*/
+			/*
+			 * driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			 * driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			 */
 
 		} else if (applicationurl.equalsIgnoreCase("Prod")) {
 			driver.get(prop.getProperty("produrl"));
